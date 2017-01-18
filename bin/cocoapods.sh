7b949fdb2ebc438417192a6054f30f264b4ae4c8
Joe Motacek <cleanshooter@gmail.com>
2017-01-18 09:27:38 -0600

    testing fix for windows link fails
    Signed-off-by: Joe Motacek <cleanshooter@gmail.com>

--- bin/cocoapods.sh
+++ bin/cocoapods.sh
@@ -1,48 +1,57 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-## https://github.com/auth0/react-native-lock/blob/master/bin/cocoapods.sh
+if [ "$(uname)" == "Darwin" ]; then
+  #!/bin/sh
 
-ios_dir=`pwd`/ios
-if [ -d ios_dir ]
-  then
-  exit 0
-fi
+  ## https://github.com/auth0/react-native-lock/blob/master/bin/cocoapods.sh
 
-podfile="$ios_dir/Podfile"
-template=`pwd`/node_modules/react-native-oauth/ios/Podfile.template
-
-echo "Checking Podfile in iOS project ($podfile)"
-
-if [ -f $podfile ]
-  then
-  echo ""
-  echo "Found an existing Podfile, Do you want to override it? [N/y]"
-  read generate_env_file
-
-  if [ "$generate_env_file" != "y" ]
+  ios_dir=`pwd`/ios
+  if [ -d ios_dir ]
     then
-  
-    echo "Add the following pods":
-    echo ""
-    echo ""
-    cat $template
-    echo ""
-    echo ""
-    echo "and run 'pod install' to install OAuth for iOS"
     exit 0
   fi
 
-  rm -f $podfile
-  rm -f "$podfile.lock"
+  podfile="$ios_dir/Podfile"
+  template=`pwd`/node_modules/react-native-oauth/ios/Podfile.template
+
+  echo "Checking Podfile in iOS project ($podfile)"
+
+  if [ -f $podfile ]
+    then
+    echo ""
+    echo "Found an existing Podfile, Do you want to override it? [N/y]"
+    read generate_env_file
+
+    if [ "$generate_env_file" != "y" ]
+      then
+    
+      echo "Add the following pods":
+      echo ""
+      echo ""
+      cat $template
+      echo ""
+      echo ""
+      echo "and run 'pod install' to install OAuth for iOS"
+      exit 0
+    fi
+
+    rm -f $podfile
+    rm -f "$podfile.lock"
+  fi
+
+  echo "Adding Podfile to iOS project"
+
+  cd ios
+  pod init >/dev/null 2>&1
+  cat $template >> $podfile
+  cd ..
+
+  echo "Installing Pods"
+
+  pod install --project-directory=ios       
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # Do something under GNU/Linux platform
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    # Do something under Windows NT platform
 fi
 
-echo "Adding Podfile to iOS project"
-
-cd ios
-pod init >/dev/null 2>&1
-cat $template >> $podfile
-cd ..
-
-echo "Installing Pods"
-
-pod install --project-directory=ios
--- bin/prepare.sh
+++ bin/prepare.sh
@@ -1,15 +1,23 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-## https://github.com/auth0/react-native-lock/blob/master/bin/prepare.sh
+if [ "$(uname)" == "Darwin" ]; then
+  #!/bin/sh
 
-echo "Preparing to link react-native-firestack for iOS"
+  ## https://github.com/auth0/react-native-lock/blob/master/bin/prepare.sh
 
-echo "Checking CocoaPods..."
-has_cocoapods=`which pod >/dev/null 2>&1`
-if [ -z "$has_cocoapods" ]
-then
-  echo "CocoaPods already installed"
-else
-  echo "Installing CocoaPods..."
-  gem install cocoapods
+  echo "Preparing to link react-native-firestack for iOS"
+
+  echo "Checking CocoaPods..."
+  has_cocoapods=`which pod >/dev/null 2>&1`
+  if [ -z "$has_cocoapods" ]
+  then
+    echo "CocoaPods already installed"
+  else
+    echo "Installing CocoaPods..."
+    gem install cocoapods
+  fi       
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # Do something under GNU/Linux platform
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    # Do something under Windows NT platform
 fi
